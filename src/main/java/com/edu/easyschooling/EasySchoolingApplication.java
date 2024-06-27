5088efd4c894f260390c4ab2409aac3bf2c6589c
Rushy77 <165278379+Rushy77@users.noreply.github.com>
2024-06-27 15:39:50 +0100

    Initial commit

---
+++ src/main/java/com/edu/easyschooling/EasySchoolingApplication.java
@@ -0,0 +1,13 @@
+package com.edu.easyschooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySchoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasySchoolingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/easyschooling/EasySchoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.easyschooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySchoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
